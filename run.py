56d4d686e499436198d5d58606d6645df9ab7944
Marcelo Silva <maarcelomonteiro@hotmail.com>
2020-06-26 21:46:58 -0300

    refactor: run file

---
+++ run.py
@@ -0,0 +1,23 @@
+﻿import speech_recognition as sr
+import os
+import services
+
+
+def run():
+
+    # enable microphone
+    microphone = sr.Recognizer()
+    with sr.Microphone() as source:
+        # noise reduction
+        microphone.adjust_for_ambient_noise(source)
+        audio = microphone.listen(source)
+    try:
+        # speech recognition
+        services.identifyCommand(
+            microphone.recognize_google(audio, language='pt-BR'))
+        run()
+    except:
+        run()
+
+
+run()
